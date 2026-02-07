28ec37c918766aa10b54e2cc6e5d791de6de6e82
RalphDante <106255342+RalphDante@users.noreply.github.com>
2026-02-07 10:30:15 +0800

    Implement basic calculator functionality in Program.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,48 @@
 ﻿//write your console display in here
+
+using System;
+
+public class Comandante
+{
+    public static void Main(string[] args)
+    {
+        Calculator calc = new Calculator();
+        string operation = "";
+        
+        while(operation != "="){
+            Console.WriteLine ("Enter first number: ");
+            int firstInput = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine ("Enter second number: ");
+            int secondInput = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine ("Choose operation (+, -, *, /, %, =): ");
+            operation = Console.ReadLine();
+            if(operation == "=") {
+                Console.WriteLine("Program Terminated");
+            };
+            
+            switch(operation){
+                case "+":
+                    Console.WriteLine(calc.Calculate(firstInput, secondInput, operation));
+                    break;
+                case "-":
+                    Console.WriteLine(calc.Calculate(firstInput, secondInput, operation));
+                    break;
+                case "*":
+                    Console.WriteLine(calc.Calculate(firstInput, secondInput, operation));
+                    break;
+                case "/":
+                         Console.WriteLine(calc.Calculate(firstInput, secondInput, operation));
+                    break;
+                case "%":
+                    Console.WriteLine(calc.Calculate(firstInput, secondInput, operation));
+                    break;
+                default:
+                    Console.WriteLine("Incorrect Operation Used, please try again");
+                    break;
+            }
+            
+        
+        
+        }
+    }
+}
